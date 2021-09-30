ec40f9c12b5baad1e9ddb9e118263b6c30ecf888
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-09-30 15:30:50 +0200

    Added Header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,37 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import colors from './../styles/colors';
+import lightLogo from '../assets/light-logo.svg';
+import darkLogo from '../assets/dark-logo.svg';
 
-const StyledLink = styled(Link)`
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const StyledLink = styled(Link)<{ $filledLink?: boolean }>`
     padding: 15px;
     color: ${colors.secondary};
     text-decoration: none;
     font-size: 18px;
-    /* ${(props) =>
-        props.$isFullLink &&
-        `color:white;border-radius:30px;background-color:${colors.primary}`} */
+    ${(props) =>
+        props.$filledLink &&
+        `color:white;border-radius:30px;background-color:${colors.primary}`}
 `;
 
 const Header = () => {
     return (
-        <nav>
-            <StyledLink to='/'>Home</StyledLink>
-            <StyledLink to='/survey/42'>Survey</StyledLink>
-            <StyledLink to='/freelances'>Freelances</StyledLink>
-        </nav>
+        <StyledHeader>
+            <img src={lightLogo} alt='Shiny logo' />
+            <nav>
+                <StyledLink to='/'>Home</StyledLink>
+                <StyledLink to='/freelances'>Profiles</StyledLink>
+                <StyledLink to='/survey/1' $filledLink>
+                    Take the test
+                </StyledLink>
+            </nav>
+        </StyledHeader>
     );
 };
 
