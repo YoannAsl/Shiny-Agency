322173c523178afaed6a4e1b678a330e7a269c9f
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-10-04 22:02:31 +0200

    Added a Footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { colors } from '../styles';
+import { useContext } from 'react';
+import { ThemeContext } from './../context/themeContext';
+
+const Footer = () => {
+    const themeContext = useContext(ThemeContext);
+
+    return (
+        <Container>
+            <NightModeButton onClick={themeContext?.toggleTheme}>
+                Changer de mode
+            </NightModeButton>
+        </Container>
+    );
+};
+
+const Container = styled.footer`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding-top: 60px;
+`;
+
+const NightModeButton = styled.button`
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: ${colors.secondary};
+`;
+
+export default Footer;
