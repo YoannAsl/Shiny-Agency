0df087c27d52480306e8ce17c24cbaff1dcb8bfe
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-10-07 11:47:12 +0200

    Added data handling in Results

--- client/src/pages/Results.tsx
+++ client/src/pages/Results.tsx
@@ -1,14 +1,71 @@
 import styled from 'styled-components';
-import { colors } from '../styles';
-import { useContext } from 'react';
+import { colors, Loader } from '../styles';
+import { useContext, useEffect, useState } from 'react';
 import { SurveyContext } from './../context/surveyContext';
+
+interface ResultType {
+    title: string;
+    description: string;
+}
+
+function formatFetchParams(answers: { [key: string]: boolean }) {
+    const answerNumbers = Object.keys(answers);
+
+    return answerNumbers.reduce((previousParams, answerNumber, index) => {
+        const isFirstParam = index === 0;
+        const separator = isFirstParam ? '' : '&';
+        return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`;
+    }, '');
+}
 
 function Results() {
     const surveyContext = useContext(SurveyContext);
-    console.log(surveyContext);
-    return (
+    const fetchParams = formatFetchParams(surveyContext!.answers);
+    const [data, setData] = useState<ResultType[]>([]);
+    const [isDataLoading, setIsDataLoading] = useState(false);
+    const [error, setError] = useState<unknown | null>(null);
+
+    useEffect(() => {
+        async function getData() {
+            setIsDataLoading(true);
+            try {
+                const res = await fetch(
+                    `http://localhost:8000/results?${fetchParams}`
+                );
+                const { resultsData } = await res.json();
+                setData(resultsData);
+            } catch (error) {
+                console.error(error);
+                setError(error);
+            } finally {
+                setIsDataLoading(false);
+            }
+        }
+        getData();
+    }, []);
+    console.log(data);
+
+    if (error) return <span>Il y a un problème</span>;
+
+    return isDataLoading ? (
+        <Loader />
+    ) : (
         <Main>
             <h1>Les compétences dont vous avez besoin : </h1>
+            {data &&
+                data.map((result, idx) => (
+                    <p key={idx}>
+                        {result.title}
+                        {idx === data.length - 1 ? '' : ','}
+                    </p>
+                ))}
+            {data &&
+                data.map((result, idx) => (
+                    <div>
+                        <p key={idx}>{result.title}</p>
+                        <p key={idx}>{result.description}</p>
+                    </div>
+                ))}
         </Main>
     );
 }
