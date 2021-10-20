e79244785e4841a9c57b0a46e42724ef5adf79ee
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-10-20 16:12:37 +0200

    Added theme styling to StyledLink

--- client/src/styles/StyledLink.ts
+++ client/src/styles/StyledLink.ts
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { colors } from './index';
 
-const StyledLink = styled(Link)<{ $filledLink?: boolean }>`
+const StyledLink = styled(Link)<{ $filledLink?: boolean; theme?: string }>`
     padding: 1rem 1.5rem;
     color: ${colors.secondary};
     text-decoration: none;
     font-size: 1.8rem;
-    ${(props) =>
-        props.$filledLink &&
-        `color:white;
-        border-radius:3rem;
-        background-color:${colors.primary}`}
+    color: ${({ theme }) => (theme === 'light' ? '#8186a0' : 'white')};
+    ${({ $filledLink }) =>
+        $filledLink &&
+        `color: white; 
+    border-radius: 3rem; 
+    background-color: ${colors.primary};`}
 `;
 
 export default StyledLink;
